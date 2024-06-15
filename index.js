840c1c49918a65548ece119613abccdfdc58c87d
Kaisar-byte <mohammadkaisarhamid@gmail.com>
2024-06-15 16:21:25 +0600

    a few things updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const e = require("express");
+
 
 // middleware
 app.use(cors());
