952f3607cafbf35d35d6dc9a61e2b374785b0c46
mohammadkaisarhamid <mohammadkaisarhamid@gmail.com>
2024-06-22 23:58:39 +0600

    jwt implemented in few apis

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser())
+
+
 
 app.get("/", (req, res) => {
   res.send("Puraton Furniture Bazar server is running ");
@@ -29,6 +37,28 @@ const client = new MongoClient(uri, {
   },
 });
 
+const logger = (req, res, next) => {
+  console.log("comming from logger middlewares", req.method, req.url);
+  next()
+}
+
+const verifyToken = (req, res, next) => {
+  // const token = req?.cookies?.token;
+  const token = req?.cookies?.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access token missing" })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access " })
+    }
+    req.user = decoded;
+    next()
+  })
+
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -38,7 +68,6 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-    console.log("working fine");
 
     const furnitureCollection = client
       .db("furnitureDB")
@@ -46,6 +75,26 @@ async function run() {
 
     const userCollection = client.db("furnitureDB").collection("user");
     const orderCollection = client.db("furnitureDB").collection("order");
+
+
+    // jwt authorizational api
+    app.post('/jwt', logger, async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none"
+        })
+        .send({ success: true })
+    })
+
+
+    app.post("/logout", logger, async (req, res) => {
+      const user = req.body;
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+    })
 
     // add new product
     app.post("/addProduct", async (req, res) => {
@@ -62,6 +111,7 @@ async function run() {
 
     app.get("/categories/:subCategory", async (req, res) => {
       const subCategory = req.params.subCategory;
+
       const query = { productCategory: { $eq: subCategory } };
       const categorywiseData = await furnitureCollection.find(query).toArray();
       res.send(categorywiseData);
@@ -80,36 +130,44 @@ async function run() {
       }
     });
 
-    app.get("/users", async (req, res) => {
+    app.get("/users", logger, verifyToken, async (req, res) => {
+      const user = req.user;
+      if (user.email !== req.user.email) {
+        res.status(403).send({ message: "forbidden access" })
+      }
+
       const cursor = userCollection.find();
       const result = await cursor.toArray();
+
       res.send(result);
     });
 
-    app.get("/users/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { email: email };
+    app.get("/users/:email", logger, verifyToken, async (req, res) => {
+      const user = req.user
+      if (user.email !== req.params.email) {
+        return res.status(403).send({ message: "forbidden access" })
+      }
+      const loggedUserEmail = req.params.email;
+      const query = { email: loggedUserEmail };
       const result = await userCollection.findOne(query);
       res.send(result);
     });
-   
 
 
-    app.post("/order", async (req, res)=>{
+    app.post("/order", async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
       res.send(result);
     })
-    app.get("/orders", async (req, res)=>{
+    app.get("/orders", logger, verifyToken, async (req, res) => {
       const cursor = orderCollection.find()
       const result = await cursor.toArray();
-      console.log(result)
       res.send(result);
     })
 
-   
+
   } finally {
-    
+
   }
 }
 run().catch(console.dir);
